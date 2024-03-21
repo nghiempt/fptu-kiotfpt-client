1e7a3ad13b6daf5cd3cddc81a558f417b4993f56
Nghiem Thanh Pham <nghiempt@Nghiems-MacBook-Pro.local>
2024-03-21 12:43:04 +0700

    fix: update

--- constant/url.ts
+++ constant/url.ts
@@ -1,5 +1,5 @@
 export const URL = {
     AVATAR: 'https://cdn-icons-png.flaticon.com/128/2202/2202112.png',
     LOGO: "https://kiotfpt.devilop.me/kiotfpt_logo.jpg",
-    BANNER: "https://kiotfpt.devilop.me/banner_01.png"
+    BANNER: "https://kiotfpt.devilop.me/banner_05.jpg"
 };
